c8c585b2fcdd78b9f4e1468bffef846ac95797c9
Filipe Bacof <filipebacof@gmail.com>
2022-09-21 18:51:18 -0300

    criada a chave para fazer requisições na API

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,3 +1,5 @@
+// API ------> https://www.themoviedb.org/
+// Abaixo a chave da API e também o inicio da URL para requisições
 const API_KEY = "3e8c384b95bad87a3aea94c7b62372ad";
 const API_BASE = "https://api.themoviedb.org/3";
 
