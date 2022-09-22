fe1ff32c19a8535537599059c3388139347de18a
Filipe Bacof <filipebacof@gmail.com>
2022-09-22 18:33:20 -0300

    início do novo componente - filme em destaque

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import "./App.css";
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -25,6 +27,10 @@ export default () => {
   // rodapé
   return (
     <div className='page'>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
       <section className="lists">
         {movieList.map((item, key) => (
           < MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react';
+import './FeaturedMovie.css';
+
+export default () => {
+    return (
+        <div>
+            filme em destaque   
+        </div>
+    );
+}
