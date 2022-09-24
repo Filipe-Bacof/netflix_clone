29facb885f5902fe6cf432a3e5b40e7e9ac56bb7
Filipe Bacof <filipebacof@gmail.com>
2022-09-24 20:38:40 -0300

    projeto Finalizado

--- src/App.js
+++ src/App.js
@@ -67,6 +67,11 @@ export default () => {
 
       <Footer />
 
+      {movieList.length <= 0 &&
+      <div className='loading'>
+          <img src='https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif' alt='Carregando...'></img>
+      </div>
+      }
     </div>
   );
 }
